7a6e8636c5607c8108a08953d6d78e289fa4c111
杨坤海 <384037191@qq.com>
2019-02-22 22:46:21 +0800

    add antd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import 'antd/dist/antd.css'; 
+import { Button } from 'antd';
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+  Hello world!
+  <Button>Default</Button>
+  <Button type="primary">Primary</Button>
+  <Button>Default</Button>
+  <Button type="dashed">Dashed</Button>
+  <Button type="danger">Danger</Button>
+  </div>
+);
