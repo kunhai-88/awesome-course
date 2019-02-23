5cd6f7fe3f8fd04f7bd9fed60ec3b63210f8c048
杨坤海 <384037191@qq.com>
2019-02-23 18:29:54 +0800

    add baidu analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-less',
-    'gatsby-plugin-robots-txt'
+    'gatsby-plugin-robots-txt',
+    {
+      resolve: 'gatsby-plugin-baidu-analytics',
+      options: {
+          // baidu analytics siteId
+        siteId: "cb11b7f874cdce3663bf22703c3e62ab",
+        // Put analytics script in the head instead of the body [default:false]
+        head: false,
+      },
+    },
   ]
 };
