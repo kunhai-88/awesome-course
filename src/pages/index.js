072793aa2dab4039cf14d2d2ae0a182604599b95
杨坤海 <384037191@qq.com>
2019-02-23 17:57:33 +0800

    add first course

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,49 @@
-import React from "react"
-import 'antd/dist/antd.css'; 
-import { Layout, Menu, Breadcrumb } from 'antd';
-import HeaderBar from '../components/header';
+import React from "react";
+import "antd/dist/antd.css";
+import { Layout, Card, Row, Col, Icon } from "antd";
+import HeaderBar from "../components/header";
+import styles from "./style.module.less";
 
 const { Header, Content, Footer } = Layout;
 
 export default () => (
   <Layout>
-    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-     <HeaderBar />
+    <Header className={styles.Header}>
+      <HeaderBar />
     </Header>
-    <Content style={{ padding: '0 50px', marginTop: 64 }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+    <Content className={styles.Content}>
+      <Card title="Javascript" className={styles.Column}>
+        <Row gutter={16}>
+          <Col span={8}>
+            <a
+              target="_blank"
+              href="https://study.163.com/course/courseMain.htm?courseId=252008"
+            >
+              <Card
+                cover={
+                  <img
+                    alt="李炎恢JavaScript教程 第一季"
+                    src="https://img-ph-mirror.nosdn.127.net/2r4IAK4JnA9s1T8PUaUASg==/6598173676331791619.png?imageView&quality=100&thumbnail=450y250"
+                  />
+                }
+                actions={[
+                  <span>
+                    <a href="https://study.163.com" target="_blank">
+                      <Icon type="link" /> 网易云课堂
+                    </a>
+                  </span>
+                ]}
+              >
+                <Card.Meta title="李炎恢JavaScript教程 第一季" />
+              </Card>
+            </a>
+          </Col>
+          <Col span={8} />
+          <Col span={8} />
+        </Row>
+      </Card>
+      <Card title="Jenkins" />
     </Content>
-    <Footer style={{ textAlign: 'center' }}>
-      Course ©2019 Created by Andy
-    </Footer>
+    <Footer className={styles.Footer}>Course ©2019 Created by Andy</Footer>
   </Layout>
 );
