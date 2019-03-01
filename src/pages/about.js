120f9361aaa15b2b9cf4274ad0f94e3f5178fd2f
杨坤海 <384037191@qq.com>
2019-03-01 09:08:05 +0800

    fix style bug

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ import styles from "./style.module.less";
 
 export default () => (
   <Layout>
-      <Card title="关于" className={styles.Column}>
+      <Card title="关于" className={styles.About}>
       Awesome Course 收集优秀的视频课程,主要涵盖前端技术(Javasctipt、React、Typescript等)，DevOps相关技术(Jenkien、Docker等).
       </Card>
   </Layout>
