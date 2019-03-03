0d661513d486a85e4d2eb948fe62d9f392ca2e03
杨坤海 <384037191@qq.com>
2019-03-03 18:43:19 +0800

    add course

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default () => (
     </Card>
     <Card title="DevOps">
       <Row gutter={16}>
-        <Col span={8}>
+        <Col span={6}>
           <Course
             title="Jenkins 训练营：基础篇"
             source="https://ke.qq.com"
@@ -37,7 +37,7 @@ export default () => (
             cover="https://10.url.cn/qqcourse_logo_ng/ajNVdqHZLLCwX4Spicz9sDfckP7wPuK01CL4Pq2LBATgylzu79tTz1a89mguuic5npOOWHicurqgTc/"
           />
         </Col>
-        <Col span={8} >
+        <Col span={6} >
           <Course
             title="Jenkins 训练营：带你玩转 Pipeline"
             source="https://ke.qq.com"
@@ -46,13 +46,22 @@ export default () => (
             cover="https://10.url.cn/qqcourse_logo_ng/ajNVdqHZLLCr1RhCMbWGibOEq1nSTCQX6CFJIY69sDXppoVtaY5W1l4MIEGqZJq0iazwG4JORuHoY/"
           />
         </Col>
-        <Col span={8} >
+        <Col span={6} >
           <Course
             title="Docker入门"
             source="https://www.imooc.com"
             sourceTitle="慕课网"
             link="https://www.imooc.com/learn/867"
             cover="https://img4.mukewang.com/597e973600011a6e06000338-240-135.jpg"
+          />
+        </Col>
+        <Col span={6} >
+          <Course
+            title="Git 本地操作"
+            source="https://www.jikexueyuan.com"
+            sourceTitle="极客时间"
+            link="https://www.jikexueyuan.com/course/479.html"
+            cover="https://a1.jikexueyuan.com/home/201502/01/e5fe/54ce3651ec725.jpg"
           />
         </Col>
         
